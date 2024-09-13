087311432cb4fb7adeb1603272099281bea298ec
kage08 <harshavardhan864.hk@gmail.com>
2024-09-13 13:28:46 -0400

    Multi param testing

--- test/test_lptm/test_segment/test_scoring.py
+++ test/test_lptm/test_segment/test_scoring.py
@@ -1,23 +1,32 @@
+import pytest
 import torch
 
 from lptm.segment.scoring import ScoringModuleAddn, ScoringModuleMult
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-
-def test_addn():
-    embed_size = 5
-    hidden_size = 10
+@pytest.mark.parametrize("embed_size", [15])
+@pytest.mark.parametrize("hidden_size", [10, 15])
+@pytest.mark.parametrize("batch_size", [1, 3, 4])
+@pytest.mark.parametrize("seq_len", [5, 1, 15])
+@pytest.mark.parametrize("device", ["cpu", "cuda"])
+def test_addn(
+    embed_size: int, hidden_size: int, batch_size: int, seq_len: int, device: str
+) -> None:
     scoring = ScoringModuleAddn(embed_size, hidden_size).to(device)
-    time_embeds = torch.randn(2, 3, embed_size).to(device)
+    time_embeds = torch.randn(batch_size, seq_len, embed_size).to(device)
     scores = scoring(time_embeds)
-    assert scores.size() == (2, 3, 3)
+    assert scores.size() == (batch_size, seq_len, seq_len)
 
 
-def test_mult():
-    embed_size = 5
-    hidden_size = 10
+@pytest.mark.parametrize("embed_size", [15])
+@pytest.mark.parametrize("hidden_size", [10, 15])
+@pytest.mark.parametrize("batch_size", [1, 3, 4])
+@pytest.mark.parametrize("seq_len", [5, 1, 15])
+@pytest.mark.parametrize("device", ["cpu", "cuda"])
+def test_mult(
+    embed_size: int, hidden_size: int, batch_size: int, seq_len: int, device: str
+) -> None:
     scoring = ScoringModuleMult(embed_size, hidden_size).to(device)
-    time_embeds = torch.randn(2, 3, embed_size).to(device)
+    time_embeds = torch.randn(batch_size, seq_len, embed_size).to(device)
     scores = scoring(time_embeds)
-    assert scores.size() == (2, 3, 3)
+    assert scores.size() == (batch_size, seq_len, seq_len)
